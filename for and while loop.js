bfa44ec63e801756273ed39ce23805b335883aa7
ashikmhs <ashikmahmud113@gmail.com>
2021-02-09 19:23:48 +0600

    Loops

---
+++ for and while loop.js
@@ -0,0 +1,19 @@
+// while loop
+var i = 1; // initialization
+
+while ( i<=5 ) { //condition
+    console.log(i);
+    i++; //update
+}
+
+console.log("End of while loop!")
+console.log("Start of for loop!");
+// for loop
+
+for (var j = 1; j<=5 ; j++) {
+    console.log(j);
+}
+
+for (var k = 5; k >=1; k-- ) {
+    console.log(k);
+}
